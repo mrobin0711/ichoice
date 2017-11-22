e2c862fd743a050a532007d1a654be849b99cf13
mrobin0711 <mrobin0711@gmail.com>
2017-11-21 20:52:34 -0500

    Finished the AJAX Repsonse

--- js/index.js
+++ js/index.js
@@ -6,4 +6,45 @@ $(document).ready(function() {
   		autoplaySpeed: 2000
 	});
 
+		//Get To Show Releases
+
+	$.ajax({
+    	url: 'data/shoes.json',
+    	type: 'GET'
+    })
+  	.done(displayShoes)
+  	.fail(function(error) {
+   		console.log(error);
+  	});
+  
+
+  	function displayShoes(response) {
+  		var releases = response.releases;
+  		var html = '';
+
+  		$(releases).each(function(i, shoe){
+  			var name = shoe.name;
+  			var date = shoe.date;
+  			var image = shoe.image;
+  			var price = shoe.price;
+  			
+
+  			html += `<div class="tile">
+				        <img src="`+ image +`" alt="">
+				  		<div class="overlay">
+				          <div class="details">
+				            <p class="name">`+ name +`</p>
+				            <p class="date">RELEASE DATE: `+ date +`</p>
+				            <p class="price">`+ price +`</p>
+				          </div>
+				         </div> 
+				       </div>`;
+
+
+  		});
+  		
+  		$('.tiles').html(html);
+
+  	}
+
 });
